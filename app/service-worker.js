670d66e621befc8d8837b1570f12e3cc506b90f9
Gustavo Apolinario <gustavoapolinario@MacBook-Pro-de-Gustavo.local>
2018-04-22 11:48:38 -0300

    Cacheando todos aquivos necessarios e limpando cache caso precise

--- app/service-worker.js
+++ app/service-worker.js
@@ -3,7 +3,15 @@ var urlsToCache = [
 	'./',
 	'css/materialize.min.css',
 	'css/mystyle.css',
-	'js/materialize.min.js'
+	'js/materialize.min.js',
+	'favicon.ico',
+	'https://code.jquery.com/jquery-3.2.1.min.js',
+	'manifest.json',
+	'fonts/roboto/Roboto-Medium.woff2',
+	'fonts/roboto/Roboto-Regular.woff2',
+	'fonts/roboto/Roboto-Light.woff2',
+	'fonts/roboto/Roboto-Regular.woff',
+	'fonts/roboto/Roboto-Light.woff',
 ];
 
 self.addEventListener('install', function(event) {
@@ -12,6 +20,20 @@ self.addEventListener('install', function(event) {
 		caches.open(CACHE_NAME)
 		.then(cache => cache.addAll(urlsToCache))
 		.then(self.skipWaiting())
+	);
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+	const currentCaches = [urlsToCache, 'runtime'];
+	event.waitUntil(
+		caches.keys().then(cacheNames => {
+			return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+		}).then(cachesToDelete => {
+			return Promise.all(cachesToDelete.map(cacheToDelete => {
+				return caches.delete(cacheToDelete);
+			}));
+		}).then(() => self.clients.claim())
 	);
 });
 
