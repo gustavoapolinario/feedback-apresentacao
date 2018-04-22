89b3c86a579bf3ce3bc0880736a32964d68553cc
Gustavo Apolinario <gustavoapolinario@MacBook-Pro-de-Gustavo.local>
2018-04-22 11:23:03 -0300

    Simplificando o service worker install

--- app/service-worker.js
+++ app/service-worker.js
@@ -10,10 +10,8 @@ self.addEventListener('install', function(event) {
 	// Perform install steps
 	event.waitUntil(
 		caches.open(CACHE_NAME)
-			.then(function(cache) {
-				console.log('Opened cache');
-				return cache.addAll(urlsToCache);
-			})
+		.then(cache => cache.addAll(urlsToCache))
+		.then(self.skipWaiting())
 	);
 });
 
