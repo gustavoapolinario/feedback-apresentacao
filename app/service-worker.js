18d060ff9bfe1718eb9955bdb0153bb5b295fdc9
Gustavo Apolinario <gustavoapolinario@MacBook-Pro-de-Gustavo.local>
2018-04-22 11:21:16 -0300

    Mudando nome de arquivos e identando com tabs

---
+++ app/service-worker.js
@@ -0,0 +1,32 @@
+var CACHE_NAME = 'v0.2';
+var urlsToCache = [
+	'/',
+	'/css/materialize.min.css',
+	'/css/mystyle.css',
+	'/js/materialize.min.js',
+	'/administracao.html'
+];
+
+self.addEventListener('install', function(event) {
+	// Perform install steps
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(function(cache) {
+				console.log('Opened cache');
+				return cache.addAll(urlsToCache);
+			})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request)
+			.then(function(response) {
+				if (response) {
+					return response;
+				}
+				return fetch(event.request);
+			}
+		)
+	);
+});
